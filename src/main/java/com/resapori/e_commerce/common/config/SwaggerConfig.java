344255a075eef9f69b68da53048df9928e01bb2e
Abdelrahman Abohashish <Abdo Abohashish@DESKTOP-73638D3>
2026-09-20 11:26:49 +0300

    fix: configure Swagger relative server URL and Spring Boot forward-headers-strategy for HTTPS reverse proxy

--- src/main/java/com/resapori/e_commerce/common/config/SwaggerConfig.java
+++ src/main/java/com/resapori/e_commerce/common/config/SwaggerConfig.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "API Documentation", version = "v1"),
+        servers = {@Server(url = "/", description = "Current Server")},
         security = @SecurityRequirement(name = "bearerAuth")
 )
 @SecurityScheme(
